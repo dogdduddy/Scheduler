71f711fe508726bc16032ac68cd7a447fc9b29fc
dogdduddy <dogdduddy@gmail.com>
2021-07-08 01:32:31 +0900

    github login test

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ArrayList<CalendarDay> calendarDayList = new ArrayList<>();
         calendarDayList.add(CalendarDay.today());
         calendarDayList.add(CalendarDay.from(2021,07,02));
-        calendarDayList.add(CalendarDay.from(2021,07,04));
+        calendarDayList.add(CalendarDay.from(2021,07,05));
         calendarDayList.add(CalendarDay.from(2021,07,06));
 
         ArrayList<Integer> dotColorList = new ArrayList<>();
