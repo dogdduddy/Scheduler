a09d8bff9d34c5da561a874afead1db94f7f7e4b
bongthu <hyeonsulee@yahoo.com>
2021-07-20 16:46:41 +0900

    Diary btn add

--- app/src/main/java/com/example/scheduler/Diary.java
+++ app/src/main/java/com/example/scheduler/Diary.java
@@ -1,14 +1,89 @@
 package com.example.scheduler;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.view.menu.MenuPopupHelper;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Diary extends AppCompatActivity {
 
+    ImageView menu_show;
+    MenuBuilder menuBuilder;
+
+    @SuppressLint({"RestrictedApi", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        menu_show = (ImageView) findViewById(R.id.menu_btn);
+        menuBuilder = new MenuBuilder(this);
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.popupmenu, menuBuilder);
+
+        menu_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MenuPopupHelper optionMenu = new MenuPopupHelper(Diary.this, menuBuilder, view);
+                optionMenu.setForceShowIcon(true);
+
+                menuBuilder.setCallback(new MenuBuilder.Callback() {
+                    @Override
+
+                    public boolean onMenuItemSelected(@NonNull MenuBuilder menu, @NonNull MenuItem item) {
+                        Intent intent;
+                        switch (item.getItemId()) {
+
+                            case R.id.diary:
+                                Toast.makeText(Diary.this, "다이어리", Toast.LENGTH_SHORT).show();
+                                return true;
+
+                            case R.id.calendar:
+                                intent = new Intent(Diary.this, MainActivity.class);
+                                startActivity(intent);
+                                return true;
+
+                            case R.id.shop:
+                                intent = new Intent(Diary.this, Shop.class);
+                                startActivity(intent);
+                                return true;
+
+                            case R.id.setting:
+                                intent = new Intent(Diary.this, Setting.class);
+                                startActivity(intent);
+                                return true;
+
+                            default:
+                                return true;
+                        }
+                    }
+                    @Override
+                    public void onMenuModeChange(@NonNull MenuBuilder menu) {
+                    }
+                });
+                optionMenu.show();
+            }
+        });
     }
 }
