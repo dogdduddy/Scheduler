63e6786422a62a6d6d11cbc29f770814e18c4fb6
bongthu <hyeonsulee@yahoo.com>
2021-07-21 01:18:10 +0900

    gallery pic can added but not saved

--- app/src/main/java/com/example/scheduler/MakeDiary.java
+++ app/src/main/java/com/example/scheduler/MakeDiary.java
@@ -2,13 +2,61 @@ package com.example.scheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.InputStream;
 
 public class MakeDiary extends AppCompatActivity {
+
+    Button button;
+    private static final int REQUEST_CODE = 0;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_diary);
+
+        imageView = findViewById(R.id.image);
+
+
+        imageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+
+                    Uri uri = data.getData();
+                    Glide.with(getApplicationContext()).load(uri).into(imageView);
+
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
