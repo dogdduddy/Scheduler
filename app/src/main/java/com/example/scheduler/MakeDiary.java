0beb43b967540333d627f6b78478730561d8e4d9
bongthu <hyeonsulee@yahoo.com>
2021-07-23 17:46:26 +0900

    diary pic can edit but not high quality

--- app/src/main/java/com/example/scheduler/MakeDiary.java
+++ app/src/main/java/com/example/scheduler/MakeDiary.java
@@ -1,66 +1,169 @@
 package com.example.scheduler;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
-import android.content.ContentResolver;
-import android.content.Context;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+public class MakeDiary extends Activity implements OnClickListener {
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_GALLERY = 1;
+    private static final int CROP_FROM_GALLERY = 2;
+    private static final int ACTION_GET_CONTENT = 3;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-public class MakeDiary extends AppCompatActivity {
-
-    private static final int REQUEST_CODE = 0; // 사진 요청 코드
-    private ImageView imageView;
+    private Uri mImageCaptureUri;
+    private ImageView mPhotoImageView;
+    private Button btn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_diary);
 
-        imageView = findViewById(R.id.image);
+        btn = (Button) findViewById(R.id.diarybtn);
+        mPhotoImageView = (ImageView) findViewById(R.id.image);
 
-        imageView.setOnClickListener(new View.OnClickListener() { // 갤러리에 요청코드 보내기
-            @Override
-            public void onClick(View v) { // 이미지 클릭 시 실행 -> 이미지를 고를 갤러리를 오픈
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_CODE);
-            }
-        });
+        btn.setOnClickListener(this);
+    }
+
+
+     // 앨범에서 이미지 가져오기
+
+    private void doTakeAlbumAction() {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_GALLERY);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                try {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-                    Uri uri = data.getData();
-                    Glide.with(getApplicationContext()).load(uri)./*override(500,500). 이미지 사이즈 조정인데 안됨*/
-                            into(imageView); // 이미지 사진 넣기
+        if (resultCode != RESULT_OK)
+            return;
 
-                } catch (Exception e) {
+        switch (requestCode) {
+            case CROP_FROM_GALLERY: {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
 
+                if (resultCode != RESULT_OK) {
+                    return;
                 }
-            } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+
+                final Bundle extras = data.getExtras();
+
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/SmartWheel/" + System.currentTimeMillis() + ".jpg";
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    mPhotoImageView.setImageBitmap(photo);
+                    break;
+                }
+
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()) {
+                    f.delete();
+                }
+                //break;
             }
+
+            case PICK_FROM_GALLERY: {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+
+                mImageCaptureUri = data.getData();
+                Log.d("SmartWheel",mImageCaptureUri.getPath().toString()); // 요건 모르겠다
+
+            }
+
+            case PICK_FROM_CAMERA: {
+                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 300); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 300); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_GALLERY); // CROP_FROM_CAMERA case문 이동
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doTakeAlbumAction();
+            }
+        };
+
+        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        };
+
+        new AlertDialog.Builder(this)
+                .setTitle("업로드할 이미지 선택")
+                .setNeutralButton("앨범선택", albumListener)
+                .setNegativeButton("취소", cancelListener)
+                .show();
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath) {
+
+        // SmartWheel 폴더를 생성하여 이미지를 저장하는 방식
+
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
+
+        if (!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+            directory_SmartWheel.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
