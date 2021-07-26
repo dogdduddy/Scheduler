5d460db03fd5df6e6727eb8e6c88f9544cdd3cda
dogdduddy <dogdduddy@gmail.com>
2021-07-26 19:35:22 +0900

    todo page setting

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Context;
 import android.content.Intent;
@@ -41,12 +44,14 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
+import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.PrimitiveIterator;
 
 // 메인 月달력과 함께 햄버거 등 툴바가 표시되는 페이지
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMonthChangedListener, OnDateSelectedListener {
@@ -60,6 +65,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     EditText editText3;
     GestureDetector gestureDetector = null;
 
+    // ViewPager2
+    private int oldPosition = 0;
+    private boolean checkCurrent = false;
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 3;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         ToolMonthInit();
         CalendarInit();
-
+        // 투두 업 패ㅐ널
         SlidingUpPanelLayout gestureView = findViewById(R.id.main_panel);
         gestureView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -113,6 +126,72 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 Log.d("MainActivity", "제스쳐 onFling");
                 return true;
+            }
+        });
+        //ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1000);
+        mPager.setOffscreenPageLimit(3);
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                int newPosition = position;
+                int compaire = newPosition - oldPosition;
+
+                super.onPageSelected(position);
+
+                Log.d("MainActivity", "old  : "+ oldPosition);
+                Log.d("MainActivity", "new  : "+ newPosition);
+
+                // 두 수의 차가 -, + 인지로 위치 확인
+                if(compaire > 0 ) {
+                    if(checkCurrent)
+                        Log.d("MainActivity", "LTR -");
+                    Log.d("MainActivity", "RTL +");
+
+                }
+                else {
+                    if(checkCurrent)
+                        Log.d("MainActivity", "RTL +");
+                    Log.d("MainActivity", "LTR -");
+                }
+
+                oldPosition = newPosition;
+                checkCurrent = false;
+            }
+        });
+
+        final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+
+        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull @NotNull View page, float position) {
+                // 좌우 미리보기 없앨려면 offset에 곱하는 2 없애기
+                float myOffset = position * -(2 * pageOffset + pageMargin);
+                if (mPager.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL) {
+                    if (ViewCompat.getLayoutDirection(mPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                        page.setTranslationX(-myOffset);
+                    } else {
+                        page.setTranslationX(myOffset);
+                    }
+                } else {
+                    page.setTranslationY(myOffset);
+                }
             }
         });
     }
